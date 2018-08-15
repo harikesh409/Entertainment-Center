ab8ab28bad70cbefe414872236b66f6580bd0cb7
p.harikesh <p.harikesh409@gmail.com>
2018-08-15 12:45:58 +0530

    updated title

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -106,7 +106,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">My favorite movies list with trailers</a>
+            <a class="navbar-brand" href="#">My Favorite Movies</a>
           </div>
         </div>
       </div>
